0a0f42e52ae5f6a096aacfd5b9419150cbd27eba
Zapta <zapta@users.noreply.github.com>
2026-02-18 20:37:55 -0800

    Updated scripts.

--- yosys/gowin-bug1/run-sim.sh
+++ yosys/gowin-bug1/run-sim.sh
@@ -15,7 +15,7 @@ rm -rf _build/tang9k
 mkdir -p _build/tang9k
 
 # Iverilog
-iverilog -g2012 -o _build/tang9k/main_tb.out -DVCD_OUTPUT=_build/tang9k/main_tb -DAPIO_SIM=1 -DINTERACTIVE_SIM -I"/Users/user/.apio/packages/oss-cad-suite/share/yosys/gowin" "/Users/user/.apio/packages/oss-cad-suite/share/yosys/gowin/cells_sim.v" main.v async_fifo/async_fifo.v async_fifo/fifomem.v async_fifo/rptr_empty.v async_fifo/sync_r2w.v async_fifo/sync_w2r.v async_fifo/wptr_full.v testing/test_utils.v main_tb.v
+iverilog -g2012 -o _build/tang9k/main_tb.out -DVCD_OUTPUT=_build/tang9k/main_tb -DAPIO_SIM=1 -DINTERACTIVE_SIM -I"/Users/user/.apio/packages/oss-cad-suite/share/yosys/gowin" "/Users/user/.apio/packages/oss-cad-suite/share/yosys/gowin/cells_sim.v" main.v pll.v async_fifo.v main_tb.v
 
 # VVP
 vvp _build/tang9k/main_tb.out -dumpfile=_build/tang9k/main_tb.vcd
